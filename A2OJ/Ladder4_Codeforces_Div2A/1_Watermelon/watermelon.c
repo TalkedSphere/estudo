73959267eb41fcf2bf18229541d17bc31ad9d64b
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-12 18:30:30 -0300

    Update watermelon.c

--- A2OJ/Ladder4_Codeforces_Div2A/1_Watermelon/watermelon.c
+++ A2OJ/Ladder4_Codeforces_Div2A/1_Watermelon/watermelon.c
@@ -1,1 +1,22 @@
+/*
+ * Autor: Frederico Santos Gonçalves
+ * Data: 12/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: Watermelon
+ * Link: https://codeforces.com/problemset/problem/4/A
+ * Dificuldade: 1
+ */
 
+#include <stdio.h>
+#define SUCESSO 0
+
+int main() {
+  int weight;
+  scanf("%d", &weight);
+
+  if(weight != 2 && weight%2 == 0) printf("YES");
+  else printf("NO");
+
+  return SUCESSO;
+}
