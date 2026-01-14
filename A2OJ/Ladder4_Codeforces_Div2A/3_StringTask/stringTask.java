5a030d0f6e12869aca7c95577d226af1a49520b2
Frederico S. Gonçalves <fredericosantos0309@gmail.com>
2026-01-13 21:20:51 -0300

    Add stringTask.java for Codeforces problem solution

---
+++ A2OJ/Ladder4_Codeforces_Div2A/3_StringTask/stringTask.java
@@ -0,0 +1,27 @@
+/*
+ * Autor: Frederico Santos Gonçalves
+ * Data: 13/01/2026
+ *
+ * Plataforma: A2OJ
+ * Problema: String Task
+ * Link: https://codeforces.com/problemset/problem/118/A
+ * Dificuldade: 2
+ */
+
+import java.util.Scanner;
+
+public class stringTask {
+  private static final Scanner scanner = new Scanner(System.in);
+  private static final String VOGAIS = "aAeEiIoOuUyY";
+
+  public static void main(String[] args) {
+    String palavra = scanner.nextLine();
+    palavra = palavra.toLowerCase();
+    
+    for(int i = 0; i < palavra.length(); i ++) {
+      if(!VOGAIS.contains(palavra.subSequence(i, i+1))) {
+        System.out.print("." + palavra.charAt(i));
+      }
+    }
+  }
+}
